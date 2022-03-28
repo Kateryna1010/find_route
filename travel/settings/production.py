bafe5b5a61e14a5ee398527336064ef97f71f8c3
Kate <kate.stepannikova@gmail.com>
2022-03-28 13:55:19 +0300

    fix bag

--- travel/settings/production.py
+++ travel/settings/production.py
@@ -31,7 +31,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['find-route-dj.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
